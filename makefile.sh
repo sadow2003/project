4b60f8e515c38e4a0b8982a056065fcaefdba473
sadow2003 <155097200+sadow2003@users.noreply.github.com>
2024-04-26 19:58:11 +0100

    makefile.sh

---
+++ makefile.sh
@@ -0,0 +1,6 @@
+touch README.md
+echo "the title of this project is project ">README.md
+current_date=$(date +%Y-%m-%d)
+echo "the date of making this project is $current_date"
+number_lignes=$(wc -l guessinggame.sh)
+echo "the number of lignes in the file guessinggames.sh is :$number_lignes"
